ab8635201e04899ca0a13ee8de06a24fa6a9d517
Hruday Bharadwaj <saihruday.08@gmail.com>
2020-11-13 17:08:03 +0530

    chngs2

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,9 @@ import "../styles/App.css";
 const App = () => {
   return (
     <>
-      <p data-ns-test>
-        I am learning React. My life is getting better.
-        <p data-ns-test="project-name">Tv Series Tracker Application. </p>
-        <p data-ns-test="project-description">
-          Track progress of tv series of different platforms at a single place.{" "}
-        </p>
+      <p data-ns-test="project-name">Tv Series Tracker Application. </p>
+      <p data-ns-test="project-description">
+        Track progress of tv series of different platforms at a single place.{" "}
       </p>
     </>
   );
