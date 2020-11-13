06149f3d6f9d7538efcf9aa79f59694378b80ee6
Hruday Bharadwaj <saihruday.08@gmail.com>
2020-11-13 17:05:03 +0530

    chngs1

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,12 @@ import "../styles/App.css";
 const App = () => {
   return (
     <>
-      <p>I am learning React. My life is getting better. </p>
-      <p data-ns-test="project-name">Tv Series Tracker Application. </p>
-      <p data-ns-test="project-description">
-        Track progress of tv series of different platforms at a single place.{" "}
+      <p data-ns-test>
+        I am learning React. My life is getting better.
+        <p data-ns-test="project-name">Tv Series Tracker Application. </p>
+        <p data-ns-test="project-description">
+          Track progress of tv series of different platforms at a single place.{" "}
+        </p>
       </p>
     </>
   );
